a50f234e9c5c1af19f24f20bac70c69b29ccd9e3
veixhen <vxwilson@hotmail.com>
2023-04-09 00:21:41 +0800

    .

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
+/////////// Initialize Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getFirestore, doc, getDoc} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB7_zWxwHufzF2Ztc3-h7XhEpTBW2LslKA",
+    authDomain: "test-7bbdd.firebaseapp.com",
+    projectId: "test-7bbdd",
+    storageBucket: "test-7bbdd.appspot.com",
+    messagingSenderId: "942999556834",
+    appId: "1:942999556834:web:2b980e669574da9f1284bb",
+    measurementId: "G-EDZ5NLDH3Q"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.get_gpt_api_key = async function(){
+    const docRef = doc(db, "cred_admin", "passcode");
+    var api = '';
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        var api = docSnap.data().gpt_api_key;
+    } else { 
+        console.log("No api key found!");
+    }
+    return api;
+}
+
+var gpt_api_key = '';
+
+await get_gpt_api_key().then(api =>{
+    gpt_api_key = api;
+});
+
 window.getLuck = async function(){
     var input = "Write two sentences randomly about my luck of the day, make it kinda funny and interesting. Then on a new line rate the luck over 10.";
     await getPrompt(input).then(message =>{
-        // console.log(message);
         var result = message['choices'][0]['message']['content'];
         document.getElementById("luck").innerHTML = result;
     });
 }
 
-window.getPrompt = async function(user_prompt='', system_prompt='', assistant_prompt='', max=50){
+window.getPrompt = async function(user_prompt='', system_prompt='', assistant_prompt='', max=100){
     if(user_prompt === ''){
         user_prompt = 'Write a sad poem about my border collie, Pepper';
     }
@@ -16,7 +52,7 @@ window.getPrompt = async function(user_prompt='', system_prompt='', assistant_pr
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer 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`
+          'Authorization': `Bearer ${gpt_api_key}`
         },
         body: JSON.stringify({
           model: 'gpt-3.5-turbo',
@@ -39,12 +75,7 @@ window.getPrompt = async function(user_prompt='', system_prompt='', assistant_pr
       })
 
     return await response.json();
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log(data);
-    //     console.log(data['choices'][0]['message']['content']);
-    //     var message = data['choices'][0]['message']['content'];
-    //     return message;
-    // });
 }
+
+
 
