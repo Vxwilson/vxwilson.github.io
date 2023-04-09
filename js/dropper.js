ba7427d552b4285e1b5d6f57d817d5f81af02405
veixhen <vxwilson@hotmail.com>
2023-04-09 22:35:07 +0800

    Improved layout

--- js/dropper.js
+++ js/dropper.js
@@ -476,7 +476,7 @@ function setCollapsible() {
           if (content.style.maxHeight){
             content.style.maxHeight = null;
           } else {
-            content.style.maxHeight = content.scrollHeight + "px";
+            content.style.maxHeight = (content.scrollHeight)+ "px";
           }
         });
       }
