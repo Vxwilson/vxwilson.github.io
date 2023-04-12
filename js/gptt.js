d01b858f7ecd3f3a3c8d0546180315580a61a054
veixhen <vxwilson@hotmail.com>
2023-04-13 03:02:07 +0800

    .

--- js/gptt.js
+++ js/gptt.js
@@ -85,3 +85,107 @@ function setupHeaderButtons() {
         document.getElementById("conversation").innerHTML = "";
     });
 }
+
+function getNextState(currentState) {
+    switch (currentState) {
+        case GPTState.NORMAL:
+            return GPTState.DAN;
+        //   case State.DAN:
+        //     return State.V3;
+        //   case State.V3:
+        //     return State.NORMAL;
+        default:
+            return GPTState.NORMAL;
+    }
+}
+
+function getTextForState(state) {
+    switch (state) {
+        case GPTState.NORMAL:
+            return "Normal";
+        case GPTState.DAN:
+            return "Dan";
+        case GPTState.V3:
+            return "V3";
+        default:
+            return "Normal";
+    }
+}
+
+function addConversation(sender, content) {
+    conversation.push({
+        sender: sender,
+        timestamp: new Date().toISOString(),
+        content: content
+    });
+}
+
+function updateConversationUI() {
+    let conversationUI = document.getElementById("conversation");
+    conversationUI.innerHTML = "";
+    conversation.forEach(message => {
+        let processedMessage = processMessage(message.content);
+        conversationUI.innerHTML += `
+        <div class="message">
+            <p class="message-sender" style="font-weight:bold; color:rgb(138, 133, 114)">${message.sender}</p>
+            <p class="message-content"> <b style="color:rgb(74, 66, 78)">></b> ${processedMessage} \n\n</p>
+        </div>
+        `;
+    });
+}
+
+function processMessage(message) {
+    // add code tags for code inside message while ``` is in message
+    while (message.includes('```')) {
+        message = message.replace('```', '<code>').replace('```', '</code>');
+    }
+    return message;
+}
+
+// summarize using preexisting summary as context and incorporate the new exchange to produce a new summary that is both concise and detailed.
+async function summarize(newUserMessage, newGPTMessage) {
+    let prompt = `
+    Please generate a very concise (but retaining important details) summary of a conversation between two subjects. 
+    You will be provided with a preexisting summary of the prior conversation and ONE new exchange between the two subjects. 
+    Your summary MUST be based on the following ruleset: 
+    use the preexisting summary as context (if existing), and incorporate the new exchange to produce a new summary that is very concise.
+
+    preexisting summary: [${currentSummary === '' ? 'n/a' : currentSummary}]
+
+    new exchange: 
+    user: ${newUserMessage}
+    gpt: ${newGPTMessage}
+    `
+
+    await getPrompt(prompt, '', '', 175, 0.7, 0.8).then(message => {
+        currentSummary = message['choices'][0]['message']['content'];
+
+        totalTokens += message['usage']['total_tokens'];
+        totalTokensThisRound += message['usage']['total_tokens'];
+
+        //log to compare prompt and output
+        console.log('prompt: \n' + prompt);
+        console.log('output: \n' + currentSummary);
+    });
+
+
+}
+
+function updateTokenUI() {
+    var pp_thousand_tokens = 0.002;
+    var token_string = `Total tokens: ${totalTokens} (${(totalTokens / 1000 * pp_thousand_tokens * 4.4).toFixed(5)}MYR) (+${totalTokensThisRound} tokens)`;
+    document.getElementById("token").innerHTML = token_string;
+}
+
+// pressing ctrl+enter will trigger the generate button
+function setUpGenerateButton() {
+    const textarea = document.getElementById('prompt');
+    const button = document.getElementById('generateButton');
+
+    textarea.addEventListener('keydown', (event) => {
+        if (event.ctrlKey && event.key === 'Enter') {
+            event.preventDefault(); // prevent default behavior of adding a new line
+            button.click(); // trigger click event on the button
+        }
+    });
+}
