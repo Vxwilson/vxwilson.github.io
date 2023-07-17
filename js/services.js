7132f7fa4fb93aa7cb3b4472ca9e07283375929c
veixhen <vxwilson@hotmail.com>
2023-07-18 01:40:36 +0800

    fixed button

---
+++ js/services.js
@@ -0,0 +1,7 @@
+function sendemail(){
+    window.location.href = "mailto:vxwilson@hotmail.com?subject=Website Design Inquiry&body=";
+}
+
+function openlinkedin(){
+    window.open("https://www.linkedin.com/in/veixhen");
+}
